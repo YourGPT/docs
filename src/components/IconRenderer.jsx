792674fed742039c3cffd4c2785925358cc19e5e
Himanshu Joshi <himanshu.skyrockets@gmail.com>
2025-04-05 23:21:31 +0530

    minor fix

--- src/components/IconRenderer.jsx
+++ src/components/IconRenderer.jsx
@@ -1,37 +1,37 @@
-// import React from 'react';
-// import * as FaIcons from 'react-icons/fa';
-// import * as FiIcons from 'react-icons/fi';
-// import * as IoIcons from 'react-icons/io';
-// import * as BsIcons from 'react-icons/bs';
-// import * as MdIcons from 'react-icons/md';
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as FiIcons from 'react-icons/fi';
+import * as IoIcons from 'react-icons/io';
+import * as BsIcons from 'react-icons/bs';
+import * as MdIcons from 'react-icons/md';
 
-// const IconLibraries = {
-//   Fa: FaIcons,
-//   Fi: FiIcons,
-//   Io: IoIcons,
-//   Bs: BsIcons,
-//   Md: MdIcons
-// };
+const IconLibraries = {
+  Fa: FaIcons,
+  Fi: FiIcons,
+  Io: IoIcons,
+  Bs: BsIcons,
+  Md: MdIcons
+};
 
-// export default function IconRenderer({ icon, size = "1.5em", className = "" }) {
-//   if (!icon || typeof icon !== 'string') {
-//     return null;
-//   }
+export default function IconRenderer({ icon, size = "1.5em", className = "" }) {
+  if (!icon || typeof icon !== 'string') {
+    return null;
+  }
 
-//   // Extract library prefix (e.g., 'Fa' from 'FaUser')
-//   const prefix = icon.slice(0, 2);
-//   const library = IconLibraries[prefix];
+  // Extract library prefix (e.g., 'Fa' from 'FaUser')
+  const prefix = icon.slice(0, 2);
+  const library = IconLibraries[prefix];
 
-//   if (!library) {
-//     return <span>Icon not found</span>;
-//   }
+  if (!library) {
+    return <span>Icon not found</span>;
+  }
 
-//   // Get the actual icon component
-//   const IconComponent = library[icon];
+  // Get the actual icon component
+  const IconComponent = library[icon];
   
-//   if (!IconComponent) {
-//     return <span>Icon not found</span>;
-//   }
+  if (!IconComponent) {
+    return <span>Icon not found</span>;
+  }
 
-//   return <IconComponent size={size} className={className} />;
-// }
+  return <IconComponent size={size} className={className} />;
+}
